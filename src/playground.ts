31f224c47f4cfa6f43e8d17ea3bfe6563ecb50f0
Richard Dixon <rvictordixon@gmail.com>
2021-10-30 22:00:23 -0400

    added notes on working supertest with jasmine

---
+++ src/playground.ts
@@ -0,0 +1,17 @@
+//Import SuperTest in the spec file.
+
+
+// import supertest from 'supertest';
+// import app from '../index';
+
+// const request = supertest(app);
+// describe('Test endpoint responses', () => {
+//     it('gets the api endpoint', async (done) => {
+//         const response = await request.get('/api');
+//         expect(response.status).toBe(200);
+//         done();
+//     }
+// )});
+
+
+// then npm run test
